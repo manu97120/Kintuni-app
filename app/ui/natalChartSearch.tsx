504cfbea89663a2f1538b3999fff2f9ce9856c45
ONELS KMPA <colonelsorder@gmail.com>
2024-04-12 05:41:37 +0200

    Date & Time Picker optimized

--- app/ui/natalChartSearch.tsx
+++ app/ui/natalChartSearch.tsx
@@ -3,8 +3,8 @@ import { DatePicker } from '@mui/x-date-pickers/DatePicker';
 import { TimePicker } from '@mui/x-date-pickers/TimePicker';
 import { LocalizationProvider } from '@mui/x-date-pickers';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
-import { DemoContainer, DemoItem } from '@mui/x-date-pickers/internals/demo';
-import dayjs from 'dayjs';
+// import { DemoContainer, DemoItem } from '@mui/x-date-pickers/internals/demo';
+// import dayjs from 'dayjs';
 
 export function ClientComponent({children}: { children: React.ReactNode }){
     return (
@@ -15,17 +15,8 @@ export function ClientComponent({children}: { children: React.ReactNode }){
 }
 export default function NatalChartForm(){
     return  (
-        <DemoContainer
-                components={[
-                'DatePicker',
-                'TimePicker'
-                ]}
-            >
-                <DemoItem label="Responsive variant">
-                    <DatePicker className='text-black bg-white' label="Basic Date picker" />
-                    <TimePicker className='text-black bg-white' label="Basic Time picker" />
-                </DemoItem>  
-            </DemoContainer>
-            
+        <>
+        <DatePicker className='text-black bg-white' label="Date picker" />
+                    <TimePicker className='text-black bg-white' label="Time picker" /></>
     );
 }
