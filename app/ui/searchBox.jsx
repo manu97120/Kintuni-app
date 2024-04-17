e0c5161d97fb45690af21d27829f0890494de486
ONELS KMPA <colonelsorder@gmail.com>
2024-04-17 04:27:54 +0200

    Form v1.4

--- app/ui/searchBox.jsx
+++ app/ui/searchBox.jsx
@@ -25,7 +25,7 @@ export function MapBoxAddressAutofill() {
 export function MapboxSearchBox() {
     // const session_token = crypto.randomUUID(); // automatic session token
     const [inputV, setInputV] = useState('');
-    // const [suggestId,setSuggestId] = useState(null);
+    const [suggestId,setSuggestId] = useState([]);
 
     const handleSearch = useDebouncedCallback((term) => {
         setInputV(term);
@@ -37,18 +37,33 @@ export function MapboxSearchBox() {
     function suggests(sugO){
         console.log(`::: SearchBoxSuggestionsResults object`);
         console.log(sugO);
+        // console.log(sugO.suggestions[0].mapbox_id);
+        // const sugId = sugO.suggestions[0].mapbox_id;
+        // setSuggestId(sugId);
+        console.log(`::: SearchBoxSuggestionsResults filter`);
+        console.log(sugO.suggestions[0].mapbox_id);
+        console.log(typeof sugO.suggestions[0].mapbox_id);
+        // console.log(suggestId);
     };
 
     function retrieve(sugO){
-        console.log(`::: SearchBoxSuggestionsResults object = ${JSON.stringify(sugO)}`);
+        console.log(`::: SearchBoxRetrieve geo coordinates`);
+        console.log(sugO);
+        console.log(`Longtitude: ${sugO.features[0].geometry.coordinates[0]} \n Lattitude: ${sugO.features[0].geometry.coordinates[1]}`);
+        // const sugId = sugO.suggestions[0].mapbox_id;
+        setSuggestId(sugO.features[0].geometry.coordinates);
+        console.log(`::: SearchBoxRetrievesResults filter`);
+        // console.log(sugO.suggestions[0].mapbox_id);
+        // console.log(typeof sugO.suggestions[0].mapbox_id);
+        console.log(suggestId);
     };
 
     return (
         // <SearchBox name="srcBox" value={value} onChange={(e)=>{target(e)}} accessToken={accessToken} />
         <SearchBox id="srcBox" name="srcBox" value={inputV} 
             accessToken={accessToken} onChange={(inputV)=>{handleSearch(inputV)}}
-            interceptSearch={inputV}
-            onSuggest={(inputV)=>{suggests(inputV)}}
+            // interceptSearch={inputV} options={types}
+            onSuggest={(inputV)=>{suggests(inputV)}} onRetrieve={(inputV)=>{retrieve(inputV)}}
         />
     );
 }
