2dade947ad4e67748f2949c8481e0568a2232601
ONELS KMPA <colonelsorder@gmail.com>
2024-05-06 07:36:14 +0200

    menu update

--- app/ui/menu.tsx
+++ app/ui/menu.tsx
@@ -3,10 +3,13 @@ import Link from "next/link";
 export default function Menu() {
   return (
     <>
-      <Link href="/">Nzo</Link>
-      <Link href="/horoscope">Horoscope</Link>
-      <Link href="/config">Config</Link>
-      <Link href="/natalchart">Natal Chart</Link>
-    </>
+  <nav className="flex flex-wrap justify-center items-center">
+    <Link href="/" className="m-2 p-2 text-gray-800 hover:text-gray-600 transition duration-300 ease-in-out">Nzo</Link>
+    <Link href="/horoscope" className="m-2 p-2 text-gray-800 hover:text-gray-600 transition duration-300 ease-in-out">Horoscope</Link>
+    <Link href="/config" className="m-2 p-2 text-gray-800 hover:text-gray-600 transition duration-300 ease-in-out">Config</Link>
+    <Link href="/natalchart" className="m-2 p-2 text-gray-800 hover:text-gray-600 transition duration-300 ease-in-out">Natal Chart</Link>
+  </nav>
+</>
+
   );
 }
